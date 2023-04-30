5db1bc1310f27829c4fc11e13a4bd0666a27a984
HerrSimoNHub <130193268+HerrSimoNHub@users.noreply.github.com>
2023-04-30 17:33:16 +0200

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,5 @@
+-- MADE BY HerrrSimoN#2115
+-- OBFUSCATE WITH SIMONHUB gg/psxscripts
+
+
+return(z]*QY:]#EQie2?#,NTFF8="Y[Y*HB-B>1,gHZr&U"w3.uFW@*_6}oO"aB=uZn9w3./^,iK$cz(=uZn9w3.z]K*EuTEd=uZn9w3.yRIB"2Cp4X="%7!q6N#.]%vQOk{"w3.CF{/+](.%="</;37hkIGn-SRS0"w3.S![K;T|;^U="=|SD}-ZIKym$?..."w3.6'P@<Dy~y="|dPg.t6yX@..."w3.7^8A$nw\s=]=180h+T5o;)m:'(/UX|:~CZE2Y9("4%b67://jK,.Ph'D,FlYmP%C8S-fr.A?;/T$H/7lUvfa}I/6>"/C'S:/p\^=~pOHP&'_-5.]A9",uZn9))()z]*QY:]#EQie2)()
